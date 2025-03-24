09a84d49b47a449802372cd59fd66325adfec4ba
Pranay4529q <114940369+Pranay4529q@users.noreply.github.com>
2025-03-24 06:45:52 +0530

    Update AudioUploader.jsx

--- frontend/src/components/AudioAnalysis/AudioUploader.jsx
+++ frontend/src/components/AudioAnalysis/AudioUploader.jsx
@@ -30,7 +30,7 @@ const AudioUploader = ({
       const formData = new FormData();
       formData.append("audio", selectedFile);
 
-      const response = await fetch("http://localhost:5000/api/predict", {
+      const response = await fetch("https://mindaware-backend.onrender.com/api/predict", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
